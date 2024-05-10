70eca333afd95e0f647112cd2937a508462afb5e
Milana Gorobchenko <40010548+milgor931@users.noreply.github.com>
2024-05-10 15:25:25 -0700

    updated jsdocs

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Function that adds two numbers.
+ * @param {number} a - The first number.
+ * @param {number} b - The second number.
+ * @returns {number} The sum of a and b.
+ */
+function add(a, b) {
+    return a + b;
+  }
+  
