cb32ed172fcd91ee84e5e502d474635523a3070d
bpanuco11 <146768736+bpanuco11@users.noreply.github.com>
2024-05-23 22:30:00 -0700

    Update day-view.js

--- js/day-view.js
+++ js/day-view.js
@@ -1,12 +1,12 @@
-const dayViewContainer = document.querySelector('.day-view');
+const dayViewContainer = document.querySelector('.day-view')
 
 // Check if there is overflow in the day-view container
-const hasOverflow = dayViewContainer.scrollHeight > dayViewContainer.clientHeight;
+const hasOverflow = dayViewContainer.scrollHeight > dayViewContainer.clientHeight
 
 // Set the position of the back button based on overflow
-const backButton = document.querySelector('.back-button');
+const backButton = document.querySelector('.back-button')
 if (hasOverflow) {
-  backButton.style.position = 'relative';
+  backButton.style.position = 'relative'
 } else {
-  backButton.style.position = 'absolute';
+  backButton.style.position = 'absolute'
 }
