3f12a7783ecf63c01c25f54fb6c1bd00ac02afe1
Julie827 <jackjack170827@gmail.com>
2024-06-05 19:18:45 -0700

    fix lint issues

--- js/character-selection.js
+++ js/character-selection.js
@@ -1,14 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
   // Select all character containers
-  const characterContainers = document.querySelectorAll('.character-container');
+  const characterContainers = document.querySelectorAll('.character-container')
   // Select the main character selection container
-  const characterSelectionContainer = document.getElementById('character-selection-container');
+  const characterSelectionContainer = document.getElementById('character-selection-container')
 
   // Add click event listener to each character container
   characterContainers.forEach(container => {
-    container.addEventListener('click', function() {
+    container.addEventListener('click', function () {
       // Add the hidden class to the main container
-      characterSelectionContainer.classList.add('hidden');
-    });
-  });
-});
+      characterSelectionContainer.classList.add('hidden')
+    })
+  })
+})
