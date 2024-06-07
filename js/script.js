0df9d0ff5c42ebae779af70ca4d22aede5df1b1f
Julie827 <jackjack170827@gmail.com>
2024-06-07 16:39:45 -0700

    Update main page

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-// Magic Numbers
-const DAY_THEME = 'day'
-const SUNSET_THEME = 'sunset'
-const NIGHT_THEME = 'night'
-
 // Add an event listener to the document that listens for mouse movement
 document.addEventListener('mousemove', function (e) {
   createStar(e.clientX, e.clientY)
@@ -24,66 +19,3 @@ function createStar (x, y) {
     star.remove()
   }, 1000)
 }
-
-// Get the elements with the IDs 'sun-moon' and 'forest-background'
-const sunMoon = document.getElementById('sun-moon')
-const forestBg = document.getElementById('forest-background')
-const stars = document.querySelectorAll('.stars') // Select all elements with the class 'stars'
-
-if (!sunMoon.style.backgroundImage) {
-  sunMoon.style.backgroundImage = "url('assets/Moon.gif')"
-}
-
-// set the theme
-function setTheme (theme) {
-  if (theme === DAY_THEME) {
-    sunMoon.style.backgroundImage = "url('assets/Sun-bright.gif')"
-    forestBg.style.background = "linear-gradient(315deg, #ffffff 0%, #abd8ff 74%), url('assets/forest.svg')"
-    stars.forEach(star => {
-      star.style.display = 'none'
-    })
-  } else if (theme === SUNSET_THEME) {
-    sunMoon.style.backgroundImage = "url('assets/Sun-dim.gif')"
-    forestBg.style.background = "linear-gradient(to bottom, #ff7e5f, #feb47b), url('assets/forest.svg')"
-  } else if (theme === NIGHT_THEME) {
-    sunMoon.style.backgroundImage = "url('assets/Moon.gif')"
-    forestBg.style.background = "linear-gradient(to bottom, blue, #feb47b), url('assets/forest.svg')"
-    stars.forEach(star => {
-      star.style.display = 'block'
-    })
-  }
-
-  forestBg.style.backgroundSize = 'cover'
-  forestBg.style.backgroundRepeat = 'no-repeat'
-  forestBg.style.backgroundPosition = 'center'
-}
-
-// Add a click event listener to the 'sun-moon' element
-sunMoon.addEventListener('click', function () {
-  const currentSunMoon = sunMoon.style.backgroundImage
-
-  // Change night to day
-  if (currentSunMoon.includes('Moon.gif')) {
-    setTheme(DAY_THEME)
-    localStorage.setItem('theme', DAY_THEME)
-  // Change day to sunset
-  } else if (currentSunMoon.includes('Sun-bright.gif')) {
-    setTheme(SUNSET_THEME)
-    localStorage.setItem('theme', SUNSET_THEME)
-  // Change sunset to night
-  } else {
-    setTheme(NIGHT_THEME)
-    localStorage.setItem('theme', NIGHT_THEME)
-  }
-})
-
-window.addEventListener('DOMContentLoaded', function () {
-  // set the theme based on what is in local storage
-  const theme = localStorage.getItem('theme')
-
-  if (theme) {
-    setTheme(theme)
-  } else {
-    setTheme(NIGHT_THEME)
-  }
-})
