07db2597b6b8fe01df585f39c409b897a888ed1e
nikothomas <nikojb@gmail.com>
2024-06-07 19:18:07 -0700

    Fixed character image/progress not updating until refresh

--- js/Character.js
+++ js/Character.js
@@ -47,6 +47,7 @@ class Character {
    * progressionStagePercentage]
    */
   getCharacterInfo () {
+    this._loadCharacter()
     const progressionPoints = this.progressionPoints
     const progressionStage = this._getProgressionStage()
     const imageAsset = this._fetchImageAsset()
